7d591e0d09bd84267152a68eee489aacd989feec
akhil <akhil.karwal@app-scoop.com>
2026-05-31 17:31:40 +0530

    seo: implement sitemap.xml and enable sitemap directive in robots.txt

--- mainweb/urls.py
+++ mainweb/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
 urlpatterns += [
     path("robots.txt", TemplateView.as_view(
         template_name="robots.txt", content_type="text/plain")),
+    path("sitemap.xml", TemplateView.as_view(
+        template_name="sitemap.xml", content_type="application/xml")),
 ]
 
 # Serve static and media files during development only
